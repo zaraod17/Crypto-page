4856c297bb99bd2dcbec80b0bfd278d47987ba54
mariusz <mariusz.waldoch98@gmail.com>
2023-01-24 19:23:13 +0100

    check

--- src/store/data_api.ts
+++ src/store/data_api.ts
@@ -1,10 +1,13 @@
 // fetching data from sandbox
 export const fetchData = async () => {
-  const response = await fetch("/cryptocurrency/listings/latest", {
-    headers: {
-      "X-CMC_PRO_API_KEY": "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c", // it's a test API key, not my own
-    },
-  });
+  const response = await fetch(
+    "https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+    {
+      headers: {
+        "X-CMC_PRO_API_KEY": "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c", // it's a test API key, not my own
+      },
+    }
+  );
 
   const responseData = await response.json();
   if (!response.ok) {
